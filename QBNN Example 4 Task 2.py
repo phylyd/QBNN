6eade140559b6ca4738b9000bcf0894510d5fb7a
phylyd <36147255+phylyd@users.noreply.github.com>
2019-10-20 13:45:05 +0800

    Update QBNN Example 4 Task 2.py

--- QBNN Example 4 Task 2.py
+++ QBNN Example 4 Task 2.py
@@ -1,33 +1,25 @@
-
 #author: Yidong Liao         yidong.liao@uq.net.au
 
-import sys
-import numpy as np
 import cmath
 import math
 
-from hiq.ops import Ph,ControlledGate,NOT,CNOT,H, Z, All, R, Swap, Measure, X, get_inverse, QFT, Tensor, BasicGate
-from hiq.meta import Loop, Compute, Uncompute, Control
+from projectq.ops import Ph,ControlledGate,NOT,CNOT,H, Z, All, R, Swap, Measure, X, get_inverse, QFT, Tensor, BasicGate
+from projectq.meta import Loop, Compute, Uncompute, Control
 
-from hiq.backends import CircuitDrawer, CommandPrinter, Simulator
-from hiq.cengines import (MainEngine,
+from projectq.cengines import (MainEngine,
                                AutoReplacer,
                                LocalOptimizer,
                                TagRemover,
                                DecompositionRuleSet)
-import ctypes
-ctypes.CDLL("libmpi.so", mode=ctypes.RTLD_GLOBAL)
-
-from huawei.hiq.cengines import GreedyScheduler
-import hiq.setups.decompositions
+from hiq.projectq.cengines import GreedyScheduler, HiQMainEngine
+from hiq.projectq.backends import SimulatorMPI
+import projectq.setups.decompositions
+from mpi4py import MPI
 
 theta = math.pi/8    #the incremental in phase accumulation
 
 def qbn(eng): 
-  
- """The operations in Quantum Binary Neurons (QBNs): multiplications between weights and inputs are performed by CNOTs, 
- addition and activation are done by a series multi-controlled gates"""
-  
+    
   #operations in the 1st layer
     CNOT | (layer1_weight_reg[0],layer1_input_reg[0])
     CNOT | (layer1_weight_reg[1],layer1_input_reg[1])  
@@ -125,13 +117,13 @@ def run_qnn(eng):
     X|layer1_input_reg[3]
     X|layer1_input_reg[2]
     X|layer1_input_reg[5]
-    #X|des_output
+    
     qnn(eng)
     X|layer1_input_reg[0]
     X|layer1_input_reg[3]
     X|layer1_input_reg[2]
     X|layer1_input_reg[5]
-    #X|des_output
+    
 
     X|layer1_input_reg[2]
     X|layer1_input_reg[5]
@@ -151,7 +143,7 @@ def run_qnn(eng):
     X|layer1_input_reg[4]
     X|layer1_input_reg[2]
     X|layer1_input_reg[5]
-    #X|des_output
+    
     qnn(eng)
     X|layer1_input_reg[0]
     X|layer1_input_reg[3]
@@ -159,11 +151,10 @@ def run_qnn(eng):
     X|layer1_input_reg[4]
     X|layer1_input_reg[2]
     X|layer1_input_reg[5]
-    #X|des_output
+   
 
     
-# Phase estimation process to evaluate the accumulated phase, namely the goodness of each weight
-# with the precision as 0.000*2pi for this instance
+
 def quanutm_phase_estimation(eng):
 
     All(H) | phase_reg
@@ -219,60 +210,64 @@ def run_qbnn(eng):
     add_minus_sign(eng)
     
     diffusion(eng)
-    
+     
 
 if __name__ == "__main__":
-    
-    eng = MainEngine(backend = Simulator(rnd_seed = 11))  #set up the simulator
+
+    backend = SimulatorMPI(gate_fusion=True, num_local_qubits=23)
+
+    cache_depth = 10
+    rule_set = DecompositionRuleSet(modules=[projectq.setups.decompositions])
+    engines = [TagRemover()
+               , LocalOptimizer(cache_depth)
+               , AutoReplacer(rule_set)
+               , TagRemover()
+               , LocalOptimizer(cache_depth)
+               , GreedyScheduler()
+               ]
+
+    eng = HiQMainEngine(backend, engines)
+
+    if MPI.COMM_WORLD.Get_rank() == 0:
 
     #allocate all the qubits
-    layer1_weight_reg = eng.allocate_qureg(6)   
-    layer1_input_reg = eng.allocate_qureg(6)
+       layer1_weight_reg = eng.allocate_qureg(6)   
+       layer1_input_reg = eng.allocate_qureg(6)
 
-    layer2_weight_reg = eng.allocate_qureg(2)
+       layer2_weight_reg = eng.allocate_qureg(2)
 
 
-    output_reg = eng.allocate_qureg(3)
-    des_output = eng.allocate_qubit()
-    ancilla_qubit = eng.allocate_qubit()
-    ancilla2 = eng.allocate_qubit()
-    phase_reg = eng.allocate_qureg(3)
-    
+       output_reg = eng.allocate_qureg(3)
+       des_output = eng.allocate_qubit()
+       ancilla_qubit = eng.allocate_qubit()
+       ancilla2 = eng.allocate_qubit()
+       phase_reg = eng.allocate_qureg(3)
+        
     #initialize the ancilla and weight qubits
-    X | ancilla_qubit
-    H | ancilla_qubit
+       X | ancilla_qubit
+       H | ancilla_qubit
 
-    All(H) | layer1_weight_reg
-    All(H) | layer2_weight_reg
+       All(H) | layer1_weight_reg
+       All(H) | layer2_weight_reg
 
-    #run the training cycle
-    with Loop(eng,5):
-        run_qbnn(eng)
+    #run the training cycle, one should adjust the number of loops and run the whole program again to get results for different iterations 
+       with Loop(eng,2):
+             run_qbnn(eng)
     
-    H | ancilla_qubit
-    X | ancilla_qubit
+       H | ancilla_qubit
+       X | ancilla_qubit
 
-    #All(Measure) | layer1_weight_reg
-    #All(Measure) | layer2_weight_reg
+       All(Measure) | layer1_weight_reg
+       All(Measure) | layer2_weight_reg
     
-    eng.flush()
+       eng.flush()
     
-    a=eng.backend.get_probability('00000000',layer1_weight_reg+layer2_weight_reg)    
-    a2=eng.backend.get_probability('11011001',layer1_weight_reg+layer2_weight_reg) 
-    a3=eng.backend.get_probability('01000010',layer1_weight_reg+layer2_weight_reg)
-    a4=eng.backend.get_probability('01100110',layer1_weight_reg+layer2_weight_reg)
-    a5=eng.backend.get_probability('11111000',layer1_weight_reg+layer2_weight_reg) 
-    
-    print("===========================================================================")
-    print("This is the QBNN demo")
-    print("The code will be updated to print the complete results, for now we print the following:) 
-    print("The probabilities of obtaining some optimal weight strings are:")
-    #print(int(layer1_weight_reg[0]),int(layer1_weight_reg[1]),int(layer1_weight_reg[2]),int(layer1_weight_reg[3]),int(layer1_weight_reg[4]),int(layer1_weight_reg[5]),int(layer2_weight_reg[0]),int(layer2_weight_reg[1]))
-    print(a)
-    print(a2)
-    print(a3)
-    print(a4)
-    print(a5)
+       print("===========================================================================")
+       print("This is the QBNN demo")
+       print("The measured weight string is")
+print(int(layer1_weight_reg[0]),int(layer1_weight_reg[1]),int(layer1_weight_reg[2]),int(layer1_weight_reg[3]),int(layer1_weight_reg[4]),int(layer1_weight_reg[5]),int(layer2_weight_reg[0]),int(layer2_weight_reg[1]))
+   
+
 
 
 
